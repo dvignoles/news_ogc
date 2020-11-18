6d8034f48a63a46e44458b26a0b1cf922504f25b
Daniel Vignoles <dvignoles@gmail.com>
2020-11-18 02:25:20 +0000

    working dev-prod docker setup

--- app/news_ogc/settings.py
+++ app/news_ogc/settings.py
@@ -38,10 +38,10 @@ INTERNAL_IPS = [
 #     os.path.join(BASE_DIR, "static"),
 # )
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
